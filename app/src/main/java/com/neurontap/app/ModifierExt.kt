a6acc2cca3321b3a339e5d618cfc7c1dda705ceb
dustyllama <40070882+dustyllama@users.noreply.github.com>
2026-09-09 08:40:07 -0500

    Match Compose offset signature

--- app/src/main/java/com/neurontap/app/ModifierExt.kt
+++ app/src/main/java/com/neurontap/app/ModifierExt.kt
@@ -2,6 +2,7 @@ package com.neurontap.app
 
 import androidx.compose.foundation.layout.offset as layoutOffset
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.IntOffset
 
-fun Modifier.offset(offset: () -> IntOffset): Modifier = this.layoutOffset(offset)
+fun Modifier.offset(offset: Density.() -> IntOffset): Modifier = this.layoutOffset(offset)
