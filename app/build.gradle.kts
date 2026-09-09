98ca6881b0198b707806c2aa54e661addea6de7e
dustyllama <40070882+dustyllama@users.noreply.github.com>
2026-09-09 14:30:10 -0500

    Bump NeuronTap to v0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.neurontap.app"
         minSdk = 28
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.4.0"
+        versionCode = 5
+        versionName = "0.5.0"
     }
 
     signingConfigs {
